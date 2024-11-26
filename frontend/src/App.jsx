7070930d48aef68149b7627350b275552c7a59f4
shri-acha <happy.irhs@gmail.com>
2024-11-26 21:45:30 +0545

    Added registration page mvp

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import LoginPage from './components/LoginPage.jsx'
-import Home from './components/Home.jsx'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from './components/LoginPage.jsx';
+import Home from './components/Home.jsx';
+import RegisterPage from "./components/RegisterPage.jsx";
+
 export default function App() {
   return(
   <>
@@ -8,6 +10,7 @@ export default function App() {
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/login" element={<LoginPage/>}/>
+      <Route path="/register" element={<RegisterPage/>}/>
     </Routes>
     </BrowserRouter>
   </>
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,5 +3,7 @@ import React from "react";
 
 export default function Home()
 {
-  return (<h1>Hello Home!</h1>);
+  return (
+    <h1>Hello Home!</h1>
+  );
 }
