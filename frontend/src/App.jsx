29322b156d160bca45884890cce3ca173d7030b2
Rochak Adhikari <rochak.adh98@gmail.com>
2024-11-29 21:28:57 +0545

    updated visibility of sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,17 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
 import Sidebar from "./components/Sidebar.jsx";
 
+import { useState } from 'react'
+
 export default function App() {
+
+  const[isSidebarVisible,setIsSidebarVisible] = useState(true);
+
   return(
     <>
-      <Navbar/>
+      <Navbar setVisibility={setIsSidebarVisible}/>
       <div className="flex h-screen bg-gray-100">
-      <Sidebar/>
+      {isSidebarVisible && <Sidebar/>}
       <Outlet/>
       </div>
     </>
