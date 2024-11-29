184fb21c2fd88dfce8eb11b7718d49f0a6d658b0
suyog-basnet <suyogbasnet2072g@gmail.com>
2024-11-29 19:42:29 +0545

    Connected Database

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -4,7 +4,7 @@ import { DB_NAME } from '../constants.js'
 export const connectToDB = async () => {
   try{
     const connectionInstance = await mongoose.connect(`${process.env.DB_URI}/${DB_NAME}`)
-    //const connectionInstance = await mongoose.connect('mongodb://127.0.0.1:27017/local_db')
+    // const connectionInstance = await mongoose.connect('mongodb://127.0.0.1:27017/KUConnect')
     console.log(`Connected to MongoDB, HOST: ${connectionInstance.connection.host}`)
   }catch(e){
     console.log(`connection error: ${e}`)
