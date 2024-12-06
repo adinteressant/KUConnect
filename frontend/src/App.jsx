07898e0d2db6c05894b583fe576b8fe8b349f88a
suyog-basnet <suyogbasnet2072g@gmail.com>
2024-12-06 20:58:06 +0545

    Merge from rochak(Leader

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,25 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
 import Sidebar from "./components/Sidebar.jsx";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function App() {
-  const [isSidebarVisible, setIsSidebarVisible] = useState(true)
-  const [paddingValue, setPaddingValue] = useState('pl-64')
+  const [isSidebarVisible, setIsSidebarVisible] = useState(true);
+  const [paddingValue, setPaddingValue] = useState("pl-64");
+
+  const location = useLocation();
+
+  useEffect(() => {
+    // Hide sidebar for login and register pages
+    if (location.pathname === "/login" || location.pathname === "/register") {
+      setIsSidebarVisible(false);
+      setPaddingValue("");
+    } else {
+      setIsSidebarVisible(true);
+      setPaddingValue("pl-64");
+    }
+  }, [location.pathname]);
 
   return (
     <>
