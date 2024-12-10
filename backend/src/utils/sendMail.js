c70427128c87072dc987a0886e0c00b5dd52426c
Rochak Adhikari <rochak.adh98@gmail.com>
2024-12-10 12:35:18 +0545

    wrote a proper email

--- backend/src/utils/sendMail.js
+++ backend/src/utils/sendMail.js
@@ -17,9 +17,15 @@ export default async function sendMail(to,otp) {
     const info = await transporter.sendMail({
       from: '"KUConnect" <kuconnect2002@gmail.com>', //sender
       to,  //receiver
-      subject:'Welcome to KUConnect', 
-      text:'Below is the verification OTP',
-      html:`<h2>${otp}</h2>`
+      subject:'Your OTP for Account Registration', 
+      text:'',
+      html:`Dear User,<br>
+      Thank you for registering with KUConnect.<br>
+      To complete your registration, please use the following One-Time Password (OTP):<br>
+      <h4>${otp}</h4><br>
+      This OTP is valid for 2 minutes. If you did not initiate this request, please disregard this email.<br><br>
+      Best regards,<br>
+      KUConnect Team`
     })
     console.log("Message sent: %s", info.messageId)
   }
