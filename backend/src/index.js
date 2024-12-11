6a83894e36a258e48bbdfe5264a9948bc105b675
suyog-basnet <suyogbasnet2072g@gmail.com>
2024-12-11 08:26:02 +0545

    Rochak Merge

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,15 @@ dotenv.config({path: './.env'});
 const app = express();
 //middleware attachments
 app.use(express.json());
-app.use(cors());
+
+app.use(
+  cors({
+    origin: 'http://localhost:5173', // Frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true, // Allow cookies to be sent with requests
+  })
+);
+
 app.use(cookieParser());
 
 const PORT = process.env.PORT || 3000
@@ -34,18 +42,20 @@ connectToDB()
 })
 
 
+// Session Setup
 app.use(session({
-  secret: 'secret',
-  resave: false,
-  saveUninitialized: false,
-  cookie:{
-    maxAge: 60000*60
-  },
-  store:MongoStore.create({
-    client:mongoose.connection.getClient()
-  })
-}))
+  secret: 'secret', // Your session secret
+  resave: false, // Don't save session if unmodified
+  saveUninitialized: false, // Don't save empty sessions
+  cookie: {
+    httpOnly: true, // Prevent client-side access to cookies
+    secure: process.env.NODE_ENV === 'production', // Set secure cookie for production
+    sameSite: 'strict', // (strict or lax)
+  }
+}));
+
 app.use(passport.initialize())
 app.use(passport.session())
 
 app.use(router)
+
