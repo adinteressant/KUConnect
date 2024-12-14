d0b52c72cadd3d9d9a6dd64b96a62624417e4f32
Rochak Adhikari <rochak.adh98@gmail.com>
2024-12-14 17:10:32 +0545

    created conversation and message models

---
+++ backend/src/models/message.model.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose'
+
+const messageSchema = new mongoose.Schema({
+  senderId:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'PublicInfo',
+    required:true
+  },
+  receiverId:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'PublicInfo',
+    required:true
+  },
+  message:{
+    type:String,
+    required:true
+  }
+},{timestamps:true}) //timestamps:true will record the time when the message was saved
+
+const Message = mongoose.model('Message',messageSchema)
+
+export default Message
