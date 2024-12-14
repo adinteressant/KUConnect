7d3dab0640d0948102f39c00423f42a0d063e74a
Aditya Pokharel <adityapokharel63@gmail.com>
2024-12-14 22:18:57 +0545

    Like icon added

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -265,16 +265,35 @@ const HomePage = () => {
                   <div className="flex items-center gap-4 mt-4">
                     <button
                       onClick = {() => handleLike(post._id.toString())}
-                      className={
-                        post.likes.some((like) => like && (like.userId === (userProfile.user_id || googleUser.user_id))) ? 'text-cyan-600' : 'text-gray-600'
-                      }
+                      className= "flex items-center gap-2 group"
                     >
-                      {post.likes.some((like) => like && (like.userId === (userProfile.user_id || googleUser.user_id))) ? 'Liked' : 'Like'}
+                      <svg width="24" height="24" viewBox="0 0 24 24"
+                      className= {post.likes.some((like) => like && (like.userId === (userProfile.user_id || googleUser.user_id)))
+                        ? 'stroke-cyan-600 fill-cyan-600 group-hover:fill-cyan-800 group-hover:stroke-cyan-800 transition-all duration-100'
+                        : 'stroke-gray-600 fill-none group-hover:fill-gray-600 transition-all duration-200'}
+                      
+                      xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
+                        />
+                      </svg>
+                      
+                      <span className = {
+                          post.likes.some((like) => like && (like.userId === (userProfile.user_id || googleUser.user_id)))
+                          ? 'text-cyan-600 group-hover:text-cyan-800 transition-all duration-100'
+                          : 'text-gray-600'
+                      }>
+                        {post.likes.some((like) => like && (like.userId === (userProfile.user_id || googleUser.user_id)))
+                        ? 'Liked' 
+                        : 'Like'
+                        }
+                      </span>
                     </button>
                     <div className="text-sm text-gray-600">
-                      {post.likes.length>0 && 
-                        (post.likes.length<3 
-                          ? `Liked by: ${post.likes.map(user => user.username).join(', ')}` 
+                      {post.likes.length>0 &&
+                        (post.likes.length<3
+                          ? `Liked by: ${post.likes.map(user => user.username).join(', ')}`
                           : `Liked by: ${post.likes.map(user => user.username).slice(-2).join(', ')} and ${post.likes.length-2} more`
                         )
                       }
