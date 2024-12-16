bbe2d3dc03b1587e5520bde6ea6a5b8f4ed1feba
suyog-basnet <suyogbasnet2072g@gmail.com>
2024-12-16 17:38:16 +0545

    Profile fixed by suggestion of leader

--- backend/src/controllers/getProfileController.js
+++ backend/src/controllers/getProfileController.js
@@ -1,26 +1,12 @@
-import PublicInfo from '../models/PublicInfo.js';
+const getProfileController = (req,res)=>{
+  
+  const {info:{role, pfp_id}} = req
 
-const getProfileController = async (req, res) => {
-  try {
-    const { username } = req.query;
-    // Fetch public info based on username
-    const publicInfo = await PublicInfo.findOne({ username });
+  res.json({
+    role:role,
+    pfp_id: pfp_id,
+    msg:'success'
+  })
+}
 
-    // If no user is found, return an error message
-    if (!publicInfo) {
-      return res.status(404).json({ msg: 'User not found' });
-    }
-
-    // Send the profile data, including role and pfp_id
-    res.json({
-      role: publicInfo.role,
-      pfp_id: publicInfo.pfp_id,
-      msg: 'success',
-    });
-  } catch (e) {
-    console.error(e);
-    res.status(500).json({ msg: 'Internal server error' });
-  }
-};
-
-export default getProfileController;
+export default getProfileController
