5655c3d372f9570e79906fef6797d4cc8ef920bb
Aditya Pokharel <adityapokharel63@gmail.com>
2024-12-18 17:52:29 +0545

    Backend - fetch comments for overlay

--- backend/src/controllers/commentController.js
+++ backend/src/controllers/commentController.js
@@ -45,3 +45,35 @@ export const addComment = async (req, res) => {
       res.status(500).json({ message: 'Error creating comment:', error })
     }
 };
+
+//Get comments for a post
+export const getComments = async(req, res) => 
+{
+  const postId = req.params.postId
+
+  try
+  {
+    const rawCommentArray = await Comment.find({ postId })
+    const commentArray = await Promise.all(
+      rawCommentArray.map(async(comment) => {
+        const user = await PublicInfo.findOne({user_id: comment.userId})
+        return {
+          pfp: user.pfp_id,
+          username: user.username,
+          role: user.role,
+          comment: comment.content,
+          created: comment.createdAt
+        }
+      })
+    )
+
+    res.status(201).json({ 
+      message: 'Comments fetched sucessfully', 
+      commentArray: commentArray })
+  }
+  catch(error)
+  {
+    console.error('Error while fetching comments:',error)
+    res.status(500).json({ message: 'Error fetching comments', error })
+  }
+}
--- backend/src/routes/commentRoutes.js
+++ backend/src/routes/commentRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express'
-import { addComment } from '../controllers/commentController.js'
+import { addComment, getComments } from '../controllers/commentController.js'
 import { validateComment } from '../middlewares/commentMiddleware.js'
 
 const router = express.Router()
 
 // Add a comment to a post
 router.post('/api/posts/:postId/users/:userId/add-comment', validateComment, addComment)
+
+// Get comments for a post
+router.get('/api/posts/:postId/get-comments', getComments)
 
 export default router
