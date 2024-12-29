dd9834ecb5da75b931c1f6e5c761beef5d212430
Rochak Adhikari <rochak.adh98@gmail.com>
2024-12-29 18:06:04 +0545

    read and unread messages

--- backend/src/models/message.model.js
+++ backend/src/models/message.model.js
@@ -14,6 +14,10 @@ const messageSchema = new mongoose.Schema({
   message:{
     type:String,
     required:true
+  },
+  read:{
+    type:Boolean,
+    default:false
   }
 },{timestamps:true}) //timestamps:true will record the time when the message was saved
 
