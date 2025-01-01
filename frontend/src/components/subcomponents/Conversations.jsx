d550017d73509cc80a74554ea376335c44917c10
Rochak Adhikari <rochak.adh98@gmail.com>
2025-01-01 22:29:10 +0545

    unread message works fine now

--- frontend/src/components/subcomponents/Conversations.jsx
+++ frontend/src/components/subcomponents/Conversations.jsx
@@ -1,39 +1,42 @@
 import { useMemo } from 'react'
 import useConversation from '../../zustand/useConversation'
 import { useGetUnreadMessage } from '../hooks/useGetUnreadMessage'
+import useNewMessages from '../../zustand/useNewMessages'
 
 export default function Conversations({ conversations, loading }) {
   const { selectedConversation, setSelectedConversation } = useConversation()
-  const unreadMessages = useGetUnreadMessage()
+  useGetUnreadMessage()
+  const {newMessages,setNewMessages} = useNewMessages()
 
   // Memoize the count calculation for better performance
-  const enhancedConversations = useMemo(() => {
-    return conversations.map((conversation) => {
-      let count = unreadMessages.filter(
-        (unreadMessage) => unreadMessage.senderId === conversation.user_id
-      ).length;
-
-      if (selectedConversation?.user_id === conversation.user_id) count = 0
-
-      return { ...conversation, count }
-    })
-  }, [conversations, unreadMessages, selectedConversation])
-
-  const changeMessageStatus = (id) => {
-    fetch(`/api/change-message-status/${id}`, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data)
+    const enhancedConversations = useMemo(() => {
+      return conversations.map((conversation) => {
+        let count = newMessages.filter(
+          (unreadMessage) => unreadMessage.senderId === conversation.user_id
+        ).length;
+  
+        if (selectedConversation?.user_id === conversation.user_id) count = 0
+  
+        return { ...conversation, count }
       })
-      .catch((e) => {
-        console.log(e)
-      });
-  };
+    }, [conversations, newMessages, selectedConversation])
+  
+    const changeMessageStatus = (id) => {
+      fetch(`/api/change-message-status/${id}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data)
+        })
+        .catch((e) => {
+          console.log(e)
+        })
+    }
+  
 
   return (
     <div className="flex flex-col gap-4 mt-5">
@@ -51,6 +54,7 @@ export default function Conversations({ conversations, loading }) {
             onClick={() => {
               setSelectedConversation(conversation)
               changeMessageStatus(conversation.user_id)
+              setNewMessages([])
             }}
           >
             <div>{conversation.username}</div>
