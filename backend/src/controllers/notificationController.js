4a9b4db54789f96bd649ad7c73ac3d9a08bd0123
shri-acha <happy.irhs@gmail.com>
2025-01-01 22:56:55 +0545

    Added some stuff,not for optimization,for refactored code

--- backend/src/controllers/notificationController.js
+++ backend/src/controllers/notificationController.js
@@ -3,19 +3,19 @@ import PrivateInfo from "../models/PrivateInfo.js";
 export default async function clearNotification(req,res) {
   const u_id = req.body.user_id?req.body.user_id:'';
   if(u_id==''){
-    return res.json({error:`User doesn't exist`}); 
+    return res.status(404).json({error:`User doesn't exist`}); 
   }
   const privateProfile =await PrivateInfo.findOne({user_id:u_id});
 
   if (!privateProfile) {
-    return res.status(500).json({ error: "Private profile not found" });
+    return res.status(404).json({ error: "Private profile not found" });
   }
-  console.log("Deleting the notifications for:",privateProfile);
+  //console.log("Deleting the notifications for:",privateProfile);
   
-  if(privateProfile.unread_count != 0) {
+  if(privateProfile.unread_count !== 0) {
   await privateProfile.updateOne({unread_count:0});
-  await privateProfile.save();  
+  //await privateProfile.save();  
   }
   
-  return res.json({body:"Success!"});
+  return res.status(200).json({message:"Success!"});
 }
--- backend/src/controllers/updateRoutesController.js
+++ backend/src/controllers/updateRoutesController.js
@@ -1,12 +1,16 @@
 import PublicInfo from '../models/PublicInfo.js';
 export default async function updateRoutesController (req,res){
  try{
-  console.log(req.body);
-  const  new_tags = req.body.tags;
+  //console.log(req.body);
+  const  new_tags = req.body.tags?req.body.tags:'';
   const user_id = req.body.user_id;
   //console.log("Request from update tags:",req.tags);
+  if (!(user_id)||user_id=='')  {
+    return res.status(401).json({error:"User does not exist!"});
+  }
   let userInfo = await PublicInfo.findOne({user_id});
-  userInfo.tags = new_tags; 
+  if (!userInfo){return res.status(500).json({error:"Internal Server Error!"})}
+   userInfo.tags = new_tags; 
   await userInfo.save();
 
   return res.status(200).json({
@@ -16,8 +20,7 @@ export default async function updateRoutesController (req,res){
 
   catch(e){
     res.status(500).json({
-      message:"Error in updating tags!",
-      e,
+      message:"Error in updating tags!", 
     })
   }
 
