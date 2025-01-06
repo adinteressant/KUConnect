fce9f93e7a13d96c562143bdc69afbc560651e7d
Aditya Pokharel <adityapokharel63@gmail.com>
2025-01-06 18:48:40 +0545

    Post deletion

--- backend/src/middlewares/postMiddleware.js
+++ backend/src/middlewares/postMiddleware.js
@@ -2,16 +2,29 @@
 
 //Middleware to validate post content
 export const validatePost = (req, res, next) => {
-  const { content } = req.body;
+  const { content } = req.body
 
   // Check if content is empty or exceeds the max length
   if (!content || content.trim().length === 0) {
-    return res.status(400).json({ message: 'Post content cannot be empty.' });
+    return res.status(400).json({ message: 'Post content cannot be empty.' })
   }
 
   if (content.length > 500) {
-    return res.status(400).json({ message: 'Post content exceeds the maximum length of 500 characters.' });
+    return res.status(400).json({ message: 'Post content exceeds the maximum length of 500 characters.' })
   }
 
-  next();
-};
+  next()
+}
+
+//Middleware to validate post deletion
+export const validatePostDeletion = (req, res, next) => {
+  const userId = req.params.userId
+  const { post } = req.body
+  
+  if(userId !== post.userId)
+  {
+    return res.status(400).json({ message: 'Post cannot be deleted by another user' })
+  }
+
+  next()
+}
