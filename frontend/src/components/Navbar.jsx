0aa328dd1bc43f1fa4699a706317be7597e94be3
Rochak Adhikari <rochak.adh98@gmail.com>
2025-01-06 15:11:56 +0545

    message customization

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -127,7 +127,7 @@ const Navigation = ({ setVisibility, setPadding,searchTrait,setSearchTrait, user
             <input
                 type="text"
                 placeholder="Search"
-                className="w-full pl-10 pr-4 py-2 rounded-full bg-gray-100 focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:bg-white transition-colors font-serif"
+                className="w-full pl-10 pr-4 py-2 rounded-full bg-gray-100 focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:bg-white transition-colors"
                 value={searchTrait}
                 onChange={(e)=>{setSearchTrait(e.target.value)}}
               />
--- frontend/src/components/subcomponents/MessageInfo.jsx
+++ frontend/src/components/subcomponents/MessageInfo.jsx
@@ -1,8 +1,10 @@
 export default function MessageInfo({isVisible}){
-  return <div className={`flex flex-col border bg-slate-50 rounded-md border-gray-200 
+  return <div className={`flex flex-col border bg-white rounded-md border-gray-200 
     ${isVisible?`absolute z-50`:`hidden`}
   `}>
-    <div className="border-b border-gray-200 p-2">messageinfo</div>
-    <div className="p-2">messageinfo2</div>
+    <div className="border-b border-gray-200 p-2 hover:bg-gray-200 cursor-pointer">
+      messageinfo
+    </div>
+    <div className="p-2 hover:bg-gray-200 cursor-pointer">messageinfo2</div>
   </div>
 }
