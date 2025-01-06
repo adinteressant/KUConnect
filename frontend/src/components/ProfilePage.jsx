d78a7baa1fecb45865c0cacf1c7421d19fd6bac3
Aditya Pokharel <adityapokharel63@gmail.com>
2025-01-06 21:18:52 +0545

    slight ui change

--- frontend/src/components/ProfilePage.jsx
+++ frontend/src/components/ProfilePage.jsx
@@ -228,7 +228,7 @@ export default function ProfilePage() {
         </div>
 
         {/* Rest of the profile content */}
-        <div className="text-center mt-4">
+        <div className="text-center mt-4 flex flex-col">
           <h1 className="text-3xl font-serif font-bold text-gray-800">
             {username}
           </h1>
@@ -237,8 +237,7 @@ export default function ProfilePage() {
           {/* Add Friend button */}
           {userProfile.username === username?
             (
-              <Link to={`/customizemyprofile`} className='flex justify-center'>
-                <div className='mt-6 bg-cyan-600 text-white rounded-full hover:shadow-lg hover:bg-cyan-700 transition-all duration-300 flex justify-center items-center gap-2 py-2 px-4'>
+              <Link to={`/customizemyprofile`} className='mx-auto mt-6 bg-cyan-600 text-white rounded-full hover:shadow-lg hover:bg-cyan-700 transition-all duration-300 flex justify-center items-center gap-2 py-2 px-4'>
                   <svg width="24" height="24" viewBox="0 0 24 24" className='fill-none stroke-2 stroke-white'>
                     <path d="M2 21a8 8 0 0 1 10.821-7.487"/>
                     <path d="M21.378 16.626a1 1 0 0 0-3.004-3.004l-4.01 4.012a2 2 0 0 0-.506.854l-.837 2.87a.5.5 0 0 0 .62.62l2.87-.837a2 2 0 0 0 .854-.506z"/>
@@ -247,7 +246,6 @@ export default function ProfilePage() {
                   <div>
                     Customize Profile
                   </div>
-                </div>
               </Link>
             )
             :
