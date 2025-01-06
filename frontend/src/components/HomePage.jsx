ad3a2841046a967d179d89ec8f8865fdfbe4145a
Aditya Pokharel <adityapokharel63@gmail.com>
2025-01-06 22:07:17 +0545

    slight ui change homepage

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -210,7 +210,7 @@ const HomePage = () => {
                 onBlur={handleTextareaBlur}
               />
 
-              <div className={`transition-all duration-300 ${isTextareaFocused || isTagsInputFocused || content.trim() || tagValue.trim() ? 'opacity-100' : 'opacity-0'}`}>
+              <div className={`transition-all duration-300 overflow-y-auto ease-in-out ${isTextareaFocused || isTagsInputFocused || content.trim() || tagValue.trim() ? 'opacity-100 max-h-screen' : 'opacity-0 max-h-0'}`}>
                 <input
                   type="text"
                   placeholder="Add tags (space-separated)"
@@ -231,6 +231,10 @@ const HomePage = () => {
                     </span>
                   ))}
                 </div>
+              </div>
+              
+              {/* Images Upload Section */}
+              <div>
               </div>
               
               <button
