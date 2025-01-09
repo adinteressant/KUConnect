ac8a03f8a6243f9decb5ec4f51918d96e748a817
shri-acha <happy.irhs@gmail.com>
2025-01-09 22:31:11 +0545

    pre merge

--- backend/src/controllers/handleWebRTC.js
+++ backend/src/controllers/handleWebRTC.js
@@ -1,37 +1,37 @@
-import pkg from 'wrtc';
-const {RTCPeerConnection , RTCSessionDescription } = pkg;
+//import pkg from 'wrtc';
+//const {RTCPeerConnection , RTCSessionDescription } = pkg;
 
 export default function handleWebRTC(io){
-  const peers = {}
-  
-  io.on('connection',(socket)=>{
-    
-    console.log("Connection Request from Client!",socket.id);
-    
-    socket.on('offer',async(id,description)=>{ // if an offer is made from a client
-      const peer = new RTCPeerConnection(); //creates a new peer and adds to peers
-      peers[id] = peer;
-      await peer.setRemoteDescription(new RTCSessionDescription(description)); // sets the other party's data as remoteDescription
-      const answer = await peer.createAnswer();
-   
-      await peer.setLocalDescription(answer); //creates an answer and sends the answer to the localDescription
-      socket.emit('answer',id,peer.localDescription); 
-    });
-    
-  socket.on('answer', async (id,description)=>{
-    const peer = peers[id];
-    await peer.setRemoteDescription(new RTCSessionDescription(description));
-    })
-
-  socket.on('candidate', async (id,candidate)=>{
-    const peer = peers[id];
-    await peer.setRemoteDescription(new RTCIceCandidate(candidate));
-    })
-
-  socket.on('disconnect',async (id)=>{
-    delete peers[id];
-    console.log('User Disconnected!');
-  })
-
-  })
-}
+//  const peers = {}
+//
+//  io.on('connection',(socket)=>{
+//
+//    console.log("Connection Request from Client!",socket.id);
+//
+//    socket.on('offer',async(id,description)=>{ // if an offer is made from a client
+//      const peer = new RTCPeerConnection(); //creates a new peer and adds to peers
+//      peers[id] = peer;
+//      await peer.setRemoteDescription(new RTCSessionDescription(description)); // sets the other party's data as remoteDescription
+//      const answer = await peer.createAnswer();
+//
+//      await peer.setLocalDescription(answer); //creates an answer and sends the answer to the localDescription
+//      socket.emit('answer',id,peer.localDescription); 
+//    });
+//
+//  socket.on('answer', async (id,description)=>{
+//    const peer = peers[id];
+//    await peer.setRemoteDescription(new RTCSessionDescription(description));
+//    })
+//
+//  socket.on('candidate', async (id,candidate)=>{
+//    const peer = peers[id];
+//    await peer.setRemoteDescription(new RTCIceCandidate(candidate));
+//    })
+//
+//  socket.on('disconnect',async (id)=>{
+//    delete peers[id];
+//    console.log('User Disconnected!');
+//  })
+//
+//  })
+//}
