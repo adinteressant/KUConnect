f735bcd6cadd2bfb585aa7c9d24f1436fc6a91a0
Rochak Adhikari <rochak.adh98@gmail.com>
2025-01-18 17:42:27 +0545

    minor issue

--- frontend/src/components/hooks/useListenMessage.js
+++ frontend/src/components/hooks/useListenMessage.js
@@ -4,11 +4,15 @@ import { useSocketContext } from '../context/socketContext'
 
 const useListenMessage = () => {
   const {socket} = useSocketContext()
-  const {messages,setMessages} = useConversation()
+  const {messages,setMessages,selectedConversation} = useConversation()
 
   useEffect(()=>{
+    
     socket?.on('newMessage',(newMessage)=>{
-      setMessages([...messages,newMessage])
+      if(selectedConversation.user_id == newMessage.senderId||
+        selectedConversation.user_id == newMessage.receiverId){
+        setMessages([...messages,newMessage])
+      }
     })
     return ()=> socket?.off('newMessage')
   },[socket,messages,setMessages])
