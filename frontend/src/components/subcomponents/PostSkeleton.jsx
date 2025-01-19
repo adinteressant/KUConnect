00a8092aa757e286f91eaf9ce53083bea96ee7fb
Aditya Pokharel <adityapokharel63@gmail.com>
2025-01-19 11:24:15 +0545

    skeleton and image transtition

---
+++ frontend/src/components/subcomponents/PostSkeleton.jsx
@@ -0,0 +1,36 @@
+export default function PostSkeleton()
+{
+    return(
+        // Post
+        <div
+            className = 'max-w-2xl mx-auto bg-white dark:bg-slate-800 dark:shadow-black dark:text-slate-200 p-4 rounded-lg shadow-md mb-4'
+        >
+            {/* Post Contents */}
+            <div className='animate-pulse mb-[25%]'>
+                {/* info of who posted */}
+                <div className='flex gap-2 dark:text-slate-200 items-evenly mb-8'>
+                    {/* profile pic */}
+                    <div className='h-9 w-9 rounded-full bg-gray-200 dark:bg-slate-900'>
+                    </div>
+                    {/* userinfo */}
+                    <div className='flex flex-col justify-between'>
+                        <div className='h-4 w-28 rounded-lg bg-gray-200 dark:bg-slate-900'>
+                        </div>
+                        <div className='h-3 w-16 rounded-lg bg-gray-200 dark:bg-slate-900'>
+                        </div>
+                    </div>
+                </div>
+                {/* Content */}
+                <div className='h-4 w-[100%] rounded-lg bg-gray-200 dark:bg-slate-900 mb-2'>
+                </div>
+                <div className='h-4 w-[100%] rounded-lg bg-gray-200 dark:bg-slate-900 mb-2'>
+                </div>
+                <div className='h-4 w-[50%] rounded-lg bg-gray-200 dark:bg-slate-900 mb-4'>
+                </div>
+                {/* tags */}
+                <div className='h-3 w-[25%] rounded-lg bg-gray-200 dark:bg-slate-900'>
+                </div>
+            </div>
+        </div>
+    )
+}
--- frontend/src/components/subcomponents/SpecificPost.jsx
+++ frontend/src/components/subcomponents/SpecificPost.jsx
@@ -1,16 +1,21 @@
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import Posts from './Posts'
+import PostSkeleton from './PostSkeleton'
 
 export default function SpecificPost()
 {
     const { postId } = useParams()
     const [posts, setPosts] = useState([])
+    const [ loadingState, setLoadingState ] = useState(true)
 
     useEffect(() =>{
         fetch(`/api/post/${postId}`)
         .then(response => response.json())
-        .then(data => setPosts(() => data.post))
+        .then(data => {
+           setPosts(() => data.post)
+            setLoadingState(() => false)
+        })
         .catch(error => {
             console.error('Error fetching post', error)
         })
@@ -18,7 +23,11 @@ export default function SpecificPost()
 
     return(
         <div className='p-6'>
+            {loadingState?
+            <PostSkeleton />
+            :
             <Posts posts={posts} setPosts={setPosts}/>
+            }
         </div>
     )
 }
