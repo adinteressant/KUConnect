b49c0ab474d77e8e0dd54ee8a85a07c6fcf4380c
Aditya Pokharel <adityapokharel63@gmail.com>
2025-01-23 15:04:50 +0545

    Loading for create and edit

--- frontend/src/components/subcomponents/PostCreateSection.jsx
+++ frontend/src/components/subcomponents/PostCreateSection.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { useOutletContext } from 'react-router-dom'
 import base64encode from '../../utils/base64encode.js'
 import tags from '../../data/tags.js'
+import { Loader2 } from 'lucide-react'
 
 export default function PostCreateSection({ parent ,user, setUser, setPosts, setPostImages, post, close }) {
   const [content, setContent] = useState(
@@ -24,11 +25,10 @@ export default function PostCreateSection({ parent ,user, setUser, setPosts, set
 
   const [createPostLoadingState, setCreatePostLoadingState] = useState(false)
   const [updatePostLoadingState, setUpdatePostLoadingState] = useState(false)
-  const { searchTrait, setSearchTrait } = useOutletContext()
 
 
   const handleTagRemove = (indexToRemove) => {
-      const newTagList = tagList.filter((tag, index) => {
+      const newTagList = tagList.filter((_, index) => {
         if(index === indexToRemove){
           return false;
         }
@@ -97,7 +97,7 @@ export default function PostCreateSection({ parent ,user, setUser, setPosts, set
           setEncodedImages([])
           setPosts(prev => [data.post, ...prev])
           setUser(user.unread_count++)
-          console.log('Post submitted:', data.post)
+          setCreatePostLoadingState(() => false)
           //location.reload()
         }
       })
@@ -147,6 +147,9 @@ export default function PostCreateSection({ parent ,user, setUser, setPosts, set
           return data.updatedPost.images===null?removed:[...removed, {...data.updatedPostImages, current: 0}]
         })
         close()
+        setTimeout(() =>{
+          setUpdatePostLoadingState(() => false)
+        }, 300)
       })
       .catch((error) => {
         console.error('Error submitting post:', error)
@@ -235,129 +238,147 @@ export default function PostCreateSection({ parent ,user, setUser, setPosts, set
     <div
       className={`dark:bg-slate-800 bg-white dark:shadow-black p-4 rounded-lg shadow-md transition-all duration-300 h-auto`}
     >
-      <textarea
-        placeholder="What's on your mind?"
-        className={`w-full p-2 border rounded-lg dark:bg-slate-900 dark:border-slate-800 dark:text-gray-200 bg-gray-100 focus:bg-white focus:outline-none focus:ring-2 focus:ring-cyan-600 transition-all duration-300 ${isTextareaFocused || isTagsInputFocused ? 'h-28' : 'h-20'
-          }`}
-        value={content}
-        onChange={(e) => setContent(e.target.value)}
-        onFocus={handleTextareaFocus}
-        onBlur={handleTextareaBlur}
-      />
-
-      <div className={`transition-all duration-500 ease-in-out ${isTextareaFocused || isTagsInputFocused || content.trim() || tagValue.trim() || tagList.length > 0 || images.length > 0 || parent ==='edit' ? 'opacity-100 max-h-screen' : 'opacity-0 max-h-0 overflow-hidden'}`}>
-        {/*Tag Input Section*/}
-        <div className='flex flex-col relative'>
-          <input
-            type="text"
-            placeholder="Add tags (space-separated)"
-            className="mt-4 flex-1 p-2 border rounded-lg dark:text-gray-200 dark:bg-slate-900 dark:border-slate-800 bg-gray-100 focus:m-1 focus:bg-white focus:outline-none focus:ring-2 focus:ring-cyan-600 transition-all duration-300"
-            value={tagValue}
-            onChange={handleTagInputChange}
-            onKeyDown={handleTagInputKeyDown}
-            onFocus={handleTagInputFocus}
-            onBlur={handleTagInputBlur}
-          />
-
-          <div className={`absolute top-full mt-1 w-full bg-gray-100 dark:bg-slate-900 rounded-lg shadow-lg z-10 transition-all duration-300 ${showTagDropdown?'max-h-60 overflow-y-auto':'max-h-0 overflow-hidden'}`}>
-            {tags
-              .filter(tag => (!tagList.includes(tag) && tag.toUpperCase().includes(tagValue.toUpperCase())))
-              .map((tag, index) => (
-                <button
-                  key={index}
-                  className="text-left w-full px-4 py-2 hover:bg-gray-200 dark:hover:bg-slate-700 dark:text-gray-200 transition-all duration-300"
-                  onClick={() => handleTagSelection(tag)}
-                >
-                  {tag}
-                </button>
-              ))}
-          </div>
-          
-          <div className="mt-2 flex flex-wrap gap-2">
-            {tagList.map((tag, index) => (
-              <span
-                key={index}
-                className="inline-flex items-center bg-cyan-100 text-cyan-800 px-2 py-1 rounded-full text-sm"
-                onClick={() => handleTagRemove(index)}
-              >
-                <span>{tag}</span>
-
-                <svg 
-                  className="w-3 h-3 inline-block" 
-                  viewBox="0 0 24 24" 
-                  fill="none" 
-                  stroke="currentColor" 
-                  strokeWidth="2"
-                >
-                  <path d="M18 6 6 18" />
-                  <path d="m6 6 12 12" />
-                </svg>
-              </span>
-            ))}
-          </div>
-        </div>
-
-        {/* Images Upload Section */}
-        <div className='mt-4'>
-          <input
-            type='file'
-            accept='image/*'
-            multiple
-            onChange={(e) => handleImageChange(e)}
-            className='hidden'
-            id={parent==='edit'?'edit-image-upload':'image-upload'}
-          />
-          <div className='flex flex-wrap gap-2'>
-            {images.map((image, index) => (
-              <div key={index} className='relative'>
-                <img src={image} alt='Preview' className='w-20 h-20 object-cover rounded-lg' />
-                <button onClick={() => handleImageRemove(index)} className='p-0.5 absolute top-0 right-0 rounded-full bg-red-600 hover:bg-red-700 transition-all duration-300'>
-                  <svg width='12' height='12' viewBox='0 0 24 24'
-                    className='stroke-2 stroke-gray-100'
-                  >
-                    <path d="M18 6 6 18" /><path d="m6 6 12 12" />
-                  </svg>
-                </button>
-              </div>))
-            }
-            {images.length >= 10 ||
-              (<label
-                htmlFor={parent==='edit'?'edit-image-upload':'image-upload'}
-                className='flex flex-col dark:bg-slate-900 dark:border-slate-700 dark:text-gray-400 items-center justify-center text-center w-20 h-20 rounded-lg text-sm border-dashed border-2 border-gray-400 text-gray-400 hover:text-cyan-600 hover:border-cyan-600 cursor-pointer transition-all duration-300'
-              >
-                Upload Images
-              </label>)
-            }
-          </div>
-        </div>
-      </div>
-      {parent==='edit'?
-      <div className='mt-4 flex justify-end gap-2'>
-        <button className='py-2 px-4 rounded-xl dark:text-gray-200 dark:hover:text-gray-300 text-gray-600 hover:text-white dark:bg-slate-700 bg-gray-200 dark:hover:bg-slate-900 hover:bg-gray-400 transition-all duration-300'
-          onClick={() => close()}>
-          Cancel
-        </button>
-        <button
-          disabled={!content.trim() || (post.content===content&&post.tags===tagList&&(post.images===null?encodedImages.length===0:post.encodedImages===encodedImages))}
-          className='py-2 px-4 rounded-xl text-white bg-cyan-600 hover:bg-cyan-700 disabled:bg-gray-400 dark:disabled:bg-slate-600 transition-all duration-300'
-          onClick={() => {
-            handlePostEdit()
-          }}
-        >
-          Update
-        </button>
+      {updatePostLoadingState?
+      <div className='w-full h-full flex justify-center items-center dark:text-gray-200 gap-2'>
+        <div>Updating Post...</div>
+        <Loader2 className='animate-spin text-cyan-600'/>
       </div>
       :
-      <button
-        disabled={!content.trim()}
-        onClick={() => { 
-          handlePostSubmit()
-        }}
-        className="mt-4 bg-cyan-600 text-white px-4 py-2 rounded-lg hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-cyan-600 focus:ring-offset-2 disabled:bg-gray-400 dark:disabled:bg-slate-600 transition-all duration-300"
-      >
-        Post
-      </button>
-      }
+      (
+        createPostLoadingState?
+        <div className='w-full h-full flex justify-center items-center dark:text-gray-200 gap-2'>
+        <div>Creating Post...</div>
+        <Loader2 className='animate-spin text-cyan-600'/>
+        </div>
+        :
+        <>
+          <textarea
+            placeholder="What's on your mind?"
+            className={`w-full p-2 border rounded-lg dark:bg-slate-900 dark:border-slate-800 dark:text-gray-200 bg-gray-100 focus:bg-white focus:outline-none focus:ring-2 focus:ring-cyan-600 transition-all duration-300 ${isTextareaFocused || isTagsInputFocused ? 'h-28' : 'h-20'
+              }`}
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            onFocus={handleTextareaFocus}
+            onBlur={handleTextareaBlur}
+          />
+
+          <div className={`transition-all duration-500 ease-in-out ${isTextareaFocused || isTagsInputFocused || content.trim() || tagValue.trim() || tagList.length > 0 || images.length > 0 || parent ==='edit' ? 'opacity-100 max-h-screen' : 'opacity-0 max-h-0 overflow-hidden'}`}>
+            {/*Tag Input Section*/}
+            <div className='flex flex-col relative'>
+              <input
+                type="text"
+                placeholder="Add tags (space-separated)"
+                className="mt-4 flex-1 p-2 border rounded-lg dark:text-gray-200 dark:bg-slate-900 dark:border-slate-800 bg-gray-100 focus:m-1 focus:bg-white focus:outline-none focus:ring-2 focus:ring-cyan-600 transition-all duration-300"
+                value={tagValue}
+                onChange={handleTagInputChange}
+                onKeyDown={handleTagInputKeyDown}
+                onFocus={handleTagInputFocus}
+                onBlur={handleTagInputBlur}
+              />
+
+              <div className={`absolute top-full mt-1 w-full bg-gray-100 dark:bg-slate-900 rounded-lg shadow-lg z-10 transition-all duration-300 ${showTagDropdown?'max-h-60 overflow-y-auto':'max-h-0 overflow-hidden'}`}>
+                {tags
+                  .filter(tag => (!tagList.includes(tag) && tag.toUpperCase().includes(tagValue.toUpperCase())))
+                  .map((tag, index) => (
+                    <button
+                      key={index}
+                      className="text-left w-full px-4 py-2 hover:bg-gray-200 dark:hover:bg-slate-700 dark:text-gray-200 transition-all duration-300"
+                      onClick={() => handleTagSelection(tag)}
+                    >
+                      {tag}
+                    </button>
+                  ))}
+              </div>
+              
+              <div className="mt-2 flex flex-wrap gap-2">
+                {tagList.map((tag, index) => (
+                  <span
+                    key={index}
+                    className="inline-flex items-center bg-cyan-100 text-cyan-800 px-2 py-1 rounded-full text-sm"
+                    onClick={() => handleTagRemove(index)}
+                  >
+                    <span>{tag}</span>
+
+                    <svg 
+                      className="w-3 h-3 inline-block" 
+                      viewBox="0 0 24 24" 
+                      fill="none" 
+                      stroke="currentColor" 
+                      strokeWidth="2"
+                    >
+                      <path d="M18 6 6 18" />
+                      <path d="m6 6 12 12" />
+                    </svg>
+                  </span>
+                ))}
+              </div>
+            </div>
+
+            {/* Images Upload Section */}
+            <div className='mt-4'>
+              <input
+                type='file'
+                accept='image/*'
+                multiple
+                onChange={(e) => handleImageChange(e)}
+                className='hidden'
+                id={parent==='edit'?'edit-image-upload':'image-upload'}
+              />
+              <div className='flex flex-wrap gap-2'>
+                {images.map((image, index) => (
+                  <div key={index} className='relative'>
+                    <img src={image} alt='Preview' className='w-20 h-20 object-cover rounded-lg' />
+                    <button onClick={() => handleImageRemove(index)} className='p-0.5 absolute top-0 right-0 rounded-full bg-red-600 hover:bg-red-700 transition-all duration-300'>
+                      <svg width='12' height='12' viewBox='0 0 24 24'
+                        className='stroke-2 stroke-gray-100'
+                      >
+                        <path d="M18 6 6 18" /><path d="m6 6 12 12" />
+                      </svg>
+                    </button>
+                  </div>))
+                }
+                {images.length >= 10 ||
+                  (<label
+                    htmlFor={parent==='edit'?'edit-image-upload':'image-upload'}
+                    className='flex flex-col dark:bg-slate-900 dark:border-slate-700 dark:text-gray-400 items-center justify-center text-center w-20 h-20 rounded-lg text-sm border-dashed border-2 border-gray-400 text-gray-400 hover:text-cyan-600 hover:border-cyan-600 cursor-pointer transition-all duration-300'
+                  >
+                    Upload Images
+                  </label>)
+                }
+              </div>
+            </div>
+          </div>
+          {parent==='edit'?
+          <div className='mt-4 flex justify-end gap-2'>
+            <button className='py-2 px-4 rounded-xl dark:text-gray-200 dark:hover:text-gray-300 text-gray-600 hover:text-white dark:bg-slate-700 bg-gray-200 dark:hover:bg-slate-900 hover:bg-gray-400 transition-all duration-300'
+              onClick={() => close()}>
+              Cancel
+            </button>
+            <button
+              disabled={!content.trim() || (post.content===content&&post.tags===tagList&&(post.images===null?encodedImages.length===0:post.encodedImages===encodedImages))}
+              className='py-2 px-4 rounded-xl text-white bg-cyan-600 hover:bg-cyan-700 disabled:bg-gray-400 dark:disabled:bg-slate-600 transition-all duration-300'
+              onClick={() => {
+                handlePostEdit()
+                setUpdatePostLoadingState(() => true)
+              }}
+            >
+              Update
+            </button>
+          </div>
+          :
+          <button
+            disabled={!content.trim()}
+            onClick={() => { 
+              handlePostSubmit()
+              setCreatePostLoadingState(() => true)
+            }}
+            className="mt-4 bg-cyan-600 text-white px-4 py-2 rounded-lg hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-cyan-600 focus:ring-offset-2 disabled:bg-gray-400 dark:disabled:bg-slate-600 transition-all duration-300"
+          >
+            Post
+          </button>
+          }
+        </>
+      )}
     </div>
   )
 }
