5eee5c48a05a5db39f8b590a4be8da014e5a220d
suyog-basnet <suyogbasnet2072g@gmail.com>
2025-01-25 18:58:52 +0545

    Dark mode to time in message and operation 2 div fixed

--- frontend/src/components/subcomponents/MessageInfo.jsx
+++ frontend/src/components/subcomponents/MessageInfo.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import useConversation from '../../zustand/useConversation'
 import { Loader2 } from 'lucide-react'
 
-export default function MessageInfo({isVisible,id}){
+export default function MessageInfo({isVisible,id,isBottomMessage}){
   const [loading,setLoading] = useState(false)
   const {selectedConversation,setMessages} = useConversation()  
   const handleDelete = async () => {
@@ -36,7 +36,7 @@ export default function MessageInfo({isVisible,id}){
     )
   }
   return <div className={`mr-[26px] flex shadow-sm flex-col border bg-white dark:bg-slate-800 rounded-md border-gray-200 dark:border-slate-700
-    ${isVisible?`absolute z-50`:`hidden`}
+    ${isVisible ? `absolute z-50 ${isBottomMessage ? 'bottom-[100%]' : 'top-[100%]'}` : 'hidden'}
   `}>
     <div className="border-b dark:hover:bg-slate-900 border-gray-200 dark:border-slate-700 p-2 hover:bg-gray-200 cursor-pointer hover:rounded-t-md"
       onClick={async () => {
