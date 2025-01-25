9e8f05e6938884f4f64870cf9d4d00b79285805c
Aditya Pokharel <adityapokharel63@gmail.com>
2025-01-25 22:42:02 +0545

    Scroll bar

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -73,7 +73,7 @@ const HomePage = () => {
   }, [])
 
   return (
-    <div className="flex-1 flex flex-col dark:bg-slate-900 bg-gray-200 overflow-y-auto">
+    <div className="flex-1 flex flex-col dark:bg-slate-900 bg-gray-200 overflow-y-auto scrollbar dark:scrollbar-dark">
       {/* Displaying user profile */}
       {(user || googleUser) && showWelcomeModal && ( 
             <WelcomeModal
