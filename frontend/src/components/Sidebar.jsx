4af84939fe8f14da33447b0e178d87b9c8e59c69
Rochak Adhikari <rochak.adh98@gmail.com>
2025-01-26 17:56:02 +0545

    bugs fixed

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -18,7 +18,7 @@ export default function Sidebar({userProfile,setUserProfile}) {
   const [isHovered, setIsHovered] = useState(false);
   const {incomingRequestsCount, setIncomingRequestsCount} = useRequestCount() 
   const {newMessages} = useNewMessages()
-  const uniqueSendersCount = new Set(newMessages.map(msg => msg.senderId)).size;
+  const uniqueSendersCount = new Set(newMessages?.map(msg => msg.senderId)).size;
   
   // const [selected, setSelected] = useState(localStorage.getItem('darkmode') || "light");
 //   const {theme, toggleTheme} = useTheme();
@@ -110,7 +110,7 @@ export default function Sidebar({userProfile,setUserProfile}) {
                           flex items-center p-3 rounded-lg cursor-pointer transition-all dark:hover:bg-gray-700`}>            
         <div className="flex gap-2 items-center">
               <div className="relative h-6 w-6 flex justify-center items-center">
-                {newMessages.length > 0 && (
+                {newMessages?.length > 0 && (
                   <div className="absolute -right-2 -top-2 flex h-5 w-5 items-center justify-center rounded-full bg-red-500 text-xs font-medium text-white">
                     {uniqueSendersCount}
                   </div>
