837155fd6ba396d9f9d62728a6671661b19d88e8
Aditya Pokharel <adityapokharel63@gmail.com>
2025-01-30 12:20:18 +0545

    Comment Model changed

--- backend/src/routes/commentRoutes.js
+++ backend/src/routes/commentRoutes.js
@@ -5,21 +5,24 @@ import { validateComment } from '../middlewares/commentMiddleware.js'
 const router = express.Router()
 
 // Add a comment to a post
-router.post('/api/posts/:postId/users/:userId/add-comment', validateComment, addComment)
+router.post('/api/post/:postId/parent/:parentId/user/:userId/add-comment', validateComment, addComment)
 
 // Get comments for a post
-router.get('/api/posts/:postId/get-comments', getComments)
+router.get('/api/post/:postId/get-comments', getComments)
 
-// Delete a comment of a post
-router.post('/api/posts/:postId/comments/:commentId/delete-comment', deleteComment)
+// Get replies for a comment
+router.get('/api/comment/:commentId', getReplies)
 
-// Add a reply to a post
-router.post('/api/posts/:postId/users/:userId/add-a-reply', validateComment, addReply)
+// // Delete a comment of a post
+// router.post('/api/posts/:postId/comments/:commentId/delete-comment', deleteComment)
 
-// Get replies for a post
-router.get('/api/comments/:commentId/get-replies', getReplies)
+// // Add a reply to a post
+// router.post('/api/posts/:postId/users/:userId/add-a-reply', validateComment, addReply)
 
-// Delete a reply for a comment
-router.post('/api/posts/:postId/comments/:commentId/delete-a-reply', deleteReply)
+// // Get replies for a post
+// router.get('/api/comments/:commentId/get-replies', getReplies)
+
+// // Delete a reply for a comment
+// router.post('/api/posts/:postId/comments/:commentId/delete-a-reply', deleteReply)
 
 export default router
