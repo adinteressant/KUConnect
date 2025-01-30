1c2a412de07078c43a748e0d91eb364a40086a72
Aditya Pokharel <adityapokharel63@gmail.com>
2025-01-30 12:44:06 +0545

    Comment ongoing

--- backend/src/routes/commentRoutes.js
+++ backend/src/routes/commentRoutes.js
@@ -10,19 +10,4 @@ router.post('/api/post/:postId/parent/:parentId/user/:userId/add-comment', valid
 // Get comments for a post
 router.get('/api/post/:postId/get-comments', getComments)
 
-// Get replies for a comment
-// router.get('/api/comment/:commentId', getReplies)
-
-// // Delete a comment of a post
-// router.post('/api/posts/:postId/comments/:commentId/delete-comment', deleteComment)
-
-// // Add a reply to a post
-// router.post('/api/posts/:postId/users/:userId/add-a-reply', validateComment, addReply)
-
-// // Get replies for a post
-// router.get('/api/comments/:commentId/get-replies', getReplies)
-
-// // Delete a reply for a comment
-// router.post('/api/posts/:postId/comments/:commentId/delete-a-reply', deleteReply)
-
 export default router
